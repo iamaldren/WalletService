c6c969d404028f5af91664ae660df5aeceec2e77
iamaldren <aldrenbobiles@gmail.com>
2019-12-18 20:42:55 +0800

    Cleaned up code

--- WalletServer/src/main/java/com/aldren/server/wallet/services/WalletService.java
+++ WalletServer/src/main/java/com/aldren/server/wallet/services/WalletService.java
@@ -3,6 +3,7 @@ package com.aldren.server.wallet.services;
 import com.aldren.server.wallet.entities.ECurrencies;
 import com.aldren.server.wallet.entities.EMoney;
 import com.aldren.server.wallet.entities.ETransactions;
+import com.aldren.server.wallet.entities.EUsers;
 import com.aldren.server.wallet.exceptions.CurrencyNotExistsException;
 import com.aldren.server.wallet.exceptions.InsufficientFundsException;
 import com.aldren.server.wallet.exceptions.UserDoesNotExistsException;
@@ -14,6 +15,7 @@ import com.aldren.server.wallet.repositories.UsersRepository;
 import com.aldren.wallet.grpc.*;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -21,6 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @GRpcService
 public class WalletService extends WalletServiceGrpc.WalletServiceImplBase {
 
@@ -42,22 +45,8 @@ public class WalletService extends WalletServiceGrpc.WalletServiceImplBase {
     @Override
     public void deposit(WalletRequest request, StreamObserver<Empty> responseObserver) {
         try {
-            currencies.findByCurrencyCode(request.getMoney().getCurrency()).ifPresentOrElse(currency -> {
-                users.findByUserId(request.getUser().getUserId()).ifPresentOrElse(user -> {
-                    money.findByUserIdAndCurrencyId(user.getId(), currency.getId()).ifPresentOrElse(amount -> {
-                        updateBalance(request.getMoney().getAmount(), CREDIT, amount);
-                        insertTransaction(request.getMoney().getAmount(), CREDIT, amount);
-                    }, () -> {
-                        EMoney newAmount = new EMoney();
-                        newAmount.setUserId(user.getId());
-                        newAmount.setCurrencyId(currency.getId());
-                        newAmount.setAmount(0d);
-                        updateBalance(request.getMoney().getAmount(), CREDIT, newAmount);
-                        insertTransaction(request.getMoney().getAmount(), CREDIT, newAmount);
-                    });
-                    responseObserver.onNext(Empty.newBuilder().build());
-                }, () -> { throw new UserDoesNotExistsException(); });
-            }, () -> { throw new CurrencyNotExistsException(); });
+            currencies.findByCurrencyCode(request.getMoney().getCurrency()).ifPresentOrElse(currency -> processByUser(request, currency, CREDIT), () -> { throw new CurrencyNotExistsException(); });
+            responseObserver.onNext(Empty.newBuilder().build());
             responseObserver.onCompleted();
         } catch (WalletException e) {
             responseObserver.onError(Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e).asException());
@@ -67,19 +56,8 @@ public class WalletService extends WalletServiceGrpc.WalletServiceImplBase {
     @Override
     public void withdraw(WalletRequest request, StreamObserver<Empty> responseObserver) {
         try {
-            currencies.findByCurrencyCode(request.getMoney().getCurrency()).ifPresentOrElse(currency -> {
-                users.findByUserId(request.getUser().getUserId()).ifPresentOrElse(user -> {
-                    money.findByUserIdAndCurrencyId(user.getId(), currency.getId()).ifPresentOrElse(amount -> {
-                        if(amount.getAmount() >= request.getMoney().getAmount()) {
-                            updateBalance(request.getMoney().getAmount(), DEBIT, amount);
-                            insertTransaction(request.getMoney().getAmount(), DEBIT, amount);
-                            responseObserver.onNext(Empty.newBuilder().build());
-                        } else {
-                            throw new InsufficientFundsException();
-                        }
-                    }, () -> { throw new CurrencyNotExistsException(request.getMoney().getCurrency()); });
-                }, () -> { throw new UserDoesNotExistsException(); });
-            }, () -> { throw new CurrencyNotExistsException(); });
+            currencies.findByCurrencyCode(request.getMoney().getCurrency()).ifPresentOrElse(currency -> processByUser(request, currency, DEBIT), () -> { throw new CurrencyNotExistsException(); });
+            responseObserver.onNext(Empty.newBuilder().build());
             responseObserver.onCompleted();
         } catch (InsufficientFundsException e) {
             responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(e.getMessage()).withCause(e).asException());
@@ -103,6 +81,37 @@ public class WalletService extends WalletServiceGrpc.WalletServiceImplBase {
         } catch (UserDoesNotExistsException e) {
             balanceStreamObserver.onError(Status.NOT_FOUND.withDescription(e.getMessage()).withCause(e).asException());
         }
+    }
+
+    private void processByUser(WalletRequest request, ECurrencies currency, String action) {
+        users.findByUserId(request.getUser().getUserId()).ifPresentOrElse(user -> processByMoney(request, user, currency, action), () -> { throw new UserDoesNotExistsException(); });
+    }
+
+    private void processByMoney(WalletRequest request, EUsers user, ECurrencies currency, String action) {
+        double amountFromRequest = request.getMoney().getAmount();
+
+        money.findByUserIdAndCurrencyId(user.getId(), currency.getId()).ifPresentOrElse(amount -> {
+            if(action.equals(DEBIT) && amount.getAmount() < amountFromRequest) {
+                throw new InsufficientFundsException();
+            }
+            updateBalance(amountFromRequest, action, amount);
+            insertTransaction(amountFromRequest, action, amount);
+        }, () -> {
+            switch (action) {
+                case CREDIT:
+                    EMoney newAmount = new EMoney();
+                    newAmount.setUserId(user.getId());
+                    newAmount.setCurrencyId(currency.getId());
+                    newAmount.setAmount(0d);
+                    updateBalance(amountFromRequest, action, newAmount);
+                    insertTransaction(amountFromRequest, action, newAmount);
+                    break;
+                case DEBIT:
+                    throw new CurrencyNotExistsException(request.getMoney().getCurrency());
+                default:
+                    log.warn("Action not supported. Program will do nothing.");
+            }
+        });
     }
 
     private void updateBalance(double amount, String action, EMoney money) {
