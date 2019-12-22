faae0b73f5534178b12a0a92df5efb99d5ef1dc7
iamaldren <aldrenbobiles@gmail.com>
2019-12-22 13:07:09 +0800

    Updated insert scripts

--- WalletServer/src/main/resources/db/migration/V2__insert_default_data.sql
+++ WalletServer/src/main/resources/db/migration/V2__insert_default_data.sql
@@ -3,3 +3,4 @@ INSERT INTO users (user_id, status) VALUES ('aldrenbobiles', 'ACTIVE');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('EUR', 'Euro');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('SGD', 'Singapore Dollar');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('PHP', 'Philippine Peso');
+INSERT INTO currencies (currency_code, currency_name) VALUES ('GBP', 'Sterling Pound');
