d9da3c3d9e52bb1898b87eb3eddf730abf8d8608
iamaldren <aldrenbobiles@gmail.com>
2019-12-22 13:29:58 +0800

    Added USD currency for insert script

--- WalletServer/src/main/resources/db/migration/V2__insert_default_data.sql
+++ WalletServer/src/main/resources/db/migration/V2__insert_default_data.sql
@@ -4,3 +4,4 @@ INSERT INTO currencies (currency_code, currency_name) VALUES ('EUR', 'Euro');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('SGD', 'Singapore Dollar');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('PHP', 'Philippine Peso');
 INSERT INTO currencies (currency_code, currency_name) VALUES ('GBP', 'Sterling Pound');
+INSERT INTO currencies (currency_code, currency_name) VALUES ('USD', 'US Dollar');
