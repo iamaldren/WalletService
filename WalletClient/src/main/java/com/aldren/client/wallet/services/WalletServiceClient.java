612e24957587db1943e966e51ec7685a5d034a5e
iamaldren <aldrenbobiles@gmail.com>
2019-12-22 11:38:45 +0800

    Wallet Client Round A

--- WalletClient/src/main/java/com/aldren/client/wallet/services/WalletServiceClient.java
+++ WalletClient/src/main/java/com/aldren/client/wallet/services/WalletServiceClient.java
@@ -2,55 +2,82 @@ package com.aldren.client.wallet.services;
 
 import com.aldren.client.wallet.constants.CurrencyConstants;
 import com.aldren.wallet.grpc.*;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @Service
 public class WalletServiceClient {
 
-    @GrpcClient("walletService")
+    @Autowired
+    private EurekaClient eurekaClient;
+
     private WalletServiceGrpc.WalletServiceBlockingStub walletService;
+
+    @PostConstruct
+    public void init() {
+        final InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("walletServerService", false);
+        final ManagedChannel channel = ManagedChannelBuilder.forAddress(instanceInfo.getIPAddr(), instanceInfo.getPort())
+                .usePlaintext()
+                .build();
+
+        walletService = WalletServiceGrpc.newBlockingStub(channel);
+    }
 
     @Async
     public void roundA(String user) {
         log.info(String.format("Round A::Executing for user %s.", user));
 
-        log.info("Round A(%s): Depositing USD 100.");
+        log.info(String.format("Round A(%s): Depositing USD 100.", user));
+        deposit(user, 100d, CurrencyConstants.CURRENCY_USD);
+
+        log.info(String.format("Round A(%s): Withdrawing USD 200.", user));
+        withdraw(user, 200d, CurrencyConstants.CURRENCY_USD);
+
+        log.info(String.format("Round A(%s): Depositing EUR 100.", user));
+        deposit(user, 100d, CurrencyConstants.CURRENCY_EUR);
+
+        checkBalance(user);
+    }
+
+    private void withdraw(String user, double amount, String currency) {
+        try {
+            walletService.withdraw(WalletRequest.newBuilder()
+                    .setUser(User.newBuilder().setUserId(user).build())
+                    .setMoney(Money.newBuilder()
+                            .setAmount(amount)
+                            .setCurrency(currency)
+                            .build())
+                    .build());
+        } catch (StatusRuntimeException e) {
+            log.info(String.format("Round A(%1s): %2s", user, e.getLocalizedMessage()));
+        }
+    }
+
+    private void deposit(String user, double amount, String currency) {
         walletService.deposit(WalletRequest.newBuilder()
                 .setUser(User.newBuilder()
                         .setUserId(user)
                         .build())
                 .setMoney(Money.newBuilder()
-                        .setAmount(100d)
-                        .setCurrency(CurrencyConstants.CURRENCY_USD)
+                        .setAmount(amount)
+                        .setCurrency(currency)
                         .build())
                 .build());
+    }
 
-        log.info("Round A(%s): Withdrawing USD 200.");
-        walletService.withdraw(WalletRequest.newBuilder()
-                .setUser(User.newBuilder().setUserId(user).build())
-                .setMoney(Money.newBuilder()
-                        .setAmount(200d)
-                        .setCurrency(CurrencyConstants.CURRENCY_USD)
-                        .build())
-                .build());
-
-        log.info("Round A(%s): Depositing EUR 100.");
-        walletService.deposit(WalletRequest.newBuilder()
-                .setUser(User.newBuilder()
-                        .setUserId(user)
-                        .build())
-                .setMoney(Money.newBuilder()
-                        .setAmount(100d)
-                        .setCurrency(CurrencyConstants.CURRENCY_EUR)
-                        .build())
-                .build());
-
+    private void checkBalance(String user) {
         Balance balance1 = walletService.checkBalance(User.newBuilder().setUserId(user).build());
-        log.info("Round A(%s): Checking balance \n" + balance1.toString());
+        log.info(String.format("Round A(%1s): Checking balance \n%2s", user, balance1.toString()));
     }
 
 }
